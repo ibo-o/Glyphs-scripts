240859587ba2feccc8012e21331c310c11717ba0
ibrahim <48119402+ibo-o@users.noreply.github.com>
2023-10-08 10:44:53 +0200

    Update ToggleTextPreviewBackground.py

--- ToggleTextPreviewBackground.py
+++ ToggleTextPreviewBackground.py
@@ -1,7 +1,7 @@
 #MenuTitle: Toggle Text Preview Background
 # -*- coding: utf-8 -*-
 __doc__="""
-To make it toggle, it must be run at least 2 times in everytime you re-open Glyphs App.
+Light Mode Users: To make it toggle, it must be run at least 2 times every time you re-open Glyphs App.
 """
 
 #Thanks to Georg Seifert
@@ -15,9 +15,9 @@ current_appearance = text_view.appearance()
 dark_appearance = NSAppearance.darkAppearance()
 light_appearance = NSAppearance.lightAppearance()
 
-if current_appearance == dark_appearance:
-    # If current appearance is dark, switch to light
-    text_view.setAppearance_(light_appearance)
-else:
+if current_appearance == light_appearance:
     # If current appearance is light, switch to dark
     text_view.setAppearance_(dark_appearance)
+else:
+    # If current appearance is dark or something else, switch to light
+    text_view.setAppearance_(light_appearance)
